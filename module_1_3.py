1f883ba6426333cf02bfa5b1008b085ab80ed8ac
Mazainsk <mazainsk@gmail.com>
2024-09-26 01:48:27 +0300

    Homework 2

--- module_1_3.py
+++ module_1_3.py
@@ -4,6 +4,6 @@ print('My name is', name, type(name))
 age = 51
 print('My integer age:', age, type(age))
 age = age + 24.0
-print('My new age is dynamically changed to float:', age, type(age))
+print('My new age was dynamically changed to float:', age, type(age))
 is_student = True
 print("Am I an old man student:", is_student, type(is_student))
