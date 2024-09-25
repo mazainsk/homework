ccf8ce41f0d704610b8ad08e1523d73da5b2ff89
Mazainsk <mazainsk@gmail.com>
2024-09-25 23:31:58 +0300

    Homework 2

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# lesson 'dynamic typing'
+name = 'Alexey'
+print('My name is', name, type(name))
+age = 51
+print('My integer age:', age, type(age))
+age = age + 24.0
+print('My new age is dynamically changed to float:', age, type(age))
+is_student = True
+print("Am I an old man student:", is_student, type(is_student))
