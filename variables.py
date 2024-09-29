2cb8f2973c77a2e3eafb9dd18c03f07d3c07858c
Mazainsk <mazainsk@gmail.com>
2024-09-29 17:57:57 +0300

    Homework "Strings 2"

--- variables.py
+++ variables.py
@@ -3,7 +3,6 @@ hours_passed = 1.5
 course_name = 'Python'
 average_time_of_homework = hours_passed / homeworks_done
 # вариант с разделителями в виде пробела (вывод с параметрами по-умолчанию):
-
 print('Курс:', course_name, ',', 'всего задач:', homeworks_done, ',', 'затрачено часов:', hours_passed, ',',
       'среднее время выполнения', average_time_of_homework, 'часа.')
 
