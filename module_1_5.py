d57a47f27c520b44a7da96ca9282ddd823516420
Mazainsk <mazainsk@gmail.com>
2024-09-29 18:47:12 +0300

    Homework "Strings 2"

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 'Земля', 'Марс', 2024, 3.4
+print('Immutable tuple:', immutable_var)
+# immutable_var[0]="Луна" # в этом месте интерпретатор выдаст ошибку из-за того, что кортеж - это неизменяемая
+# # упорядоченная коллекция
+# print(immutable_var)
+mutable_list = ['Земля', 'Марс', 2024, 3.4]
+print('Mutable list:', mutable_list)
+mutable_list[0]="Луна"
+print('New mutable list:', mutable_list)
