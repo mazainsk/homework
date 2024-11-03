f2defecbd5099a34a467a3a73d0b4b8eb71f6b03
Mazainsk <mazainsk@gmail.com>
2024-11-03 21:25:40 +0300

    Homework 5.1: House as object

---
+++ module_5_1.py
@@ -0,0 +1,27 @@
+# п.1 - создание класса:
+class House:
+
+    # п.2 - создание метода инициализации с передачей в него названия объекта и кол-ва этажей
+    def __init__(self, name, number_of_floors):
+        # п.3 - создание нужных атрибутов объекта
+        self.name = str(name)
+        self.number_of_floors = int(number_of_floors)
+        print(f'Создано здание "{self.name}" с количеством этажей {self.number_of_floors}')
+
+    # п.4 - создание метода перемещения на заданный этаж
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            print(f'В здании "{self.name}" едем с 1-го на {new_floor}-й этаж:')
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print(f'{new_floor} - такого этажа в здании "{self.name}" не существует')
+
+# Проверка на тестовых данных:
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
+h2.go_to(0)
+h2.go_to(2)
