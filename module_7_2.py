e856f8a0fbb59262bcce1fcaba73b2bb7f655e26
Mazainsk <mazainsk@gmail.com>
2024-11-18 21:43:36 +0300

    Homework 7.2: Write pos in txt file

---
+++ module_7_2.py
@@ -0,0 +1,21 @@
+# Домашнее задание по теме "Позиционирование в файле"
+
+def custom_write(file_name: str, strings: list[str]):
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as  file:
+        for i, string in enumerate(strings, 1):
+            string_begining = file.tell()
+            file.write(string + '\n')
+            strings_positions.update({(i, string_begining): string})
+    return strings_positions
+
+# Тестовые данные для проверки
+
+info = ['Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!']
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
