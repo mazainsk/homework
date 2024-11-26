1fa5e2acb33a78a3b01816979b66d155e60b8775
Mazainsk <mazainsk@gmail.com>
2024-11-27 01:25:30 +0300

    Homework 7.6: Try & Except

--- Module_7/module_7_6.py
+++ Module_7/module_7_6.py
@@ -1,13 +1,14 @@
 # Домашнее задание по теме "Try и Except"
 
-def add_everything_up2(*args):
+def add_everything_up(*args):
     """
     Функция складывает числа, строки и булевы типы, учитывает вложенные списки и кортежи,
     аргументы не подходящего типа пропускаются.
     """
     values = []
 
-    # Функция рекурсивного перестроения входных аргументов в новый ("плоский") список
+    # Вложенная функция рекурсивного перестроения входных аргументов в новый ("плоский") список
+    # с исключением из него элементов не подходящих типов
     def args_remap(*args):
         nonlocal values
         for arg in args:
@@ -17,20 +18,18 @@ def add_everything_up2(*args):
                 values.append(arg)
 
     args_remap(*args)
-    result = ''
     try:
         result = sum(values)
     except TypeError:
         result = ''.join(str(i) for i in values)
-    finally:
-        return result
+    return result
 
 
 # Проверка на тестовых данных:
 
-print(add_everything_up2(123.456, 'строка'))
-print(add_everything_up2('яблоко', 4215, '_против_груши', 48.2))
-print(add_everything_up2(123.456, 7))
-print(add_everything_up2(5, False, True))
-print(add_everything_up2(64.8, [1, 2, 3, ('?', {17, '0'}), 90], False))
-print(add_everything_up2('1', {'abc'}, '?'))
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215, '_против_груши', 48.2))
+print(add_everything_up(123.456, 7))
+print(add_everything_up(5, False, True))
+print(add_everything_up(64.8, [1, 2, 3, ('?', {17, '0'}), 90], False))
+print(add_everything_up('1', {'abc', None}, '?'))
