d371726dcecccb5bf5d8070b8f63a726865320c2
Mazainsk <mazainsk@gmail.com>
2024-12-08 05:25:25 +0300

    Homework 9.7: Using Decorators

--- Module_9/module_9_7.py
+++ Module_9/module_9_7.py
@@ -1,28 +1,38 @@
 # Домашнее задание по теме "Декораторы"
 
+import functools
+
 def is_prime(func):
+    @functools.wraps(func)
     def wrapper(*args):
         check_func = lambda n: n > 1 and all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))
-        print(f'{'Простое' if check_func(func(*args)) else 'Составное'}')
-        return func(*args)
+        text = f'{'Простое' if check_func(func(*args)) else 'Составное'}'
+        return func(*args), text
     return wrapper
 
 def is_even(func):
+    @functools.wraps(func)
     def wrapper(*args):
+        print(f'->> Вызов функции {func.__name__}() с аргументами {args}')
         orig_f = func(*args)
-        print(f'{'Четное' if not (orig_f % 2) else 'Нечетное'}, знаков {len(str(orig_f))}')
-        return orig_f
+        print(f'->> Описание: {func.__doc__}')
+        print(f'->> {func.__name__}() вернула {orig_f}')
+        text = (f'{'Четное' if not (orig_f[0] % 2) else 'Нечетное'}, знаков {len(str(orig_f[0]))}, '
+                f'сумма чисел = {sum(int(i) for i in str(orig_f[0]))}')
+        return *orig_f, text
     return wrapper
 
 # Применение декоратора is_prime (и опционально - is_even) для обертывания функции sum_three
-@is_even
+#@is_even
 @is_prime
 def sum_three(num_1, num_2, num_3):
+    """Возвращает сумму трёх чисел"""
     return sum((num_1, num_2, num_3))
 
 
 # Проверка на тестовых данных:
 res = sum_three(2, 3, 6)    # 11 - число нечетное, знаков 2, простое
-print(res)
+print(*res, sep='\n')
+print()
 res = sum_three(108, 4, 14)   # 126 - число четное, знаков 3, составное
-print(res)
+print(*res, sep='\n')
