89a07403dac5cc438aa41dff9ae206b803898efd
Mazainsk <mazainsk@gmail.com>
2024-12-13 13:19:39 +0300

    Homework 10.2: Using Thread Class

--- Module_10/module_10_2.py
+++ Module_10/module_10_2.py
@@ -22,15 +22,15 @@ class Knight(Thread):
         print(f'{self.name}, на нас напали!')
         days_of_battle = 0
         while self._Enemies > 0:
-            days_of_battle += 1
-            days_word = day_word.make_agree_with_number(days_of_battle).word
-            warriors_word = warrior_word.make_agree_with_number(self._Enemies).word
             if random.randint(1, 10) == 3 and self.power_mod == 1:
                 self.power_mod =0.8
                 print(f'{self.name} ранен! Его сила снижена на 20% до конца битвы...')
             self._Enemies -= int(self.power * self.power_mod)
             if self._Enemies < 0:
                 self._Enemies = 0
+            warriors_word = warrior_word.make_agree_with_number(self._Enemies).word
+            days_of_battle += 1
+            days_word = day_word.make_agree_with_number(days_of_battle).word
             print(f'{self.name} сражается {days_of_battle} {days_word}..., осталось {self._Enemies} {warriors_word}.')
             time.sleep(1)
             if self._Enemies == 0:
