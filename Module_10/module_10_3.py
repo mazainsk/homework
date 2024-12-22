630b9773c9a02ba267cb893952ff2cbfb9c97b57
Mazainsk <mazainsk@gmail.com>
2024-12-22 16:41:31 +0300

    Homework 10.3: Threads Blocking

--- Module_10/module_10_3.py
+++ Module_10/module_10_3.py
@@ -8,32 +8,37 @@ import threading
 class Bank:
     """Класс для симуляции операций с банковским счетом, используя операции пополнения и снятия как отдельные потоки"""
 
-    _balance: int = 0           # Текущий баланс счета
-    _lock = threading.Lock()    # Объект-замок для блокировки потоков
-    _max_operations: int = 100  # Максимальное количество успешных транзакций, отдельно для каждого вида
+    _balance: int = 0            # Текущий баланс счета
+    _lock = threading.Lock()     # Объект-замок для блокировки потоков
+    _max_operations: int = 100   # Максимальное количество успешных транзакций, отдельно для каждого вида
                                     # операций (пополнения, снятия)
-    _pause: float = 0.01        # значение задержки в секундах для имитации времени выполнения операций
+    _pause: float = 0.01         # значение задержки в секундах для имитации времени выполнения операций
+    _deposit_is_finished = False    # флаг об окончании пополнений, нужен для устранения deadlock-а
 
     # Метод совершает 100 транзакций пополнения средств
     def deposit(self):
-        for _ in range(self._max_operations):
-            with self._lock:
-                increment = random.randint(50, 500)
-                self._balance += increment
-                print(f'Пополнение: {increment}. Баланс: {self._balance}')
-                time.sleep(self._pause)
+        for i in range(self._max_operations):
+            increment = random.randint(50, 500)
+            self._balance += increment
+            print(f'Пополнение №{i+1}: {increment}. Баланс: {self._balance}')
+            time.sleep(self._pause)
+            if self._balance >= 500 and self._lock.locked():
+                self._lock.release()
+        self._deposit_is_finished = True
 
-    # Метод совершает 100 транзакций снятия средств
+    # Метод совершает 100 попыток снятия средств
     def take(self):
-        for _ in range(self._max_operations):
-            with self._lock:
-                decrement = random.randint(50, 500)
-                print(f'Запрос на {decrement}')
-                if decrement > self._balance:
-                    print('Запрос отклонён, недостаточно средств')
-                else:
-                    self._balance -= decrement
-                    print(f'Снятие: {decrement}. Баланс: {self._balance}')
+        for i in range(self._max_operations):
+            decrement = random.randint(50, 500)
+            print(f'Запрос №{i+1} на {decrement}')
+            if decrement > self._balance:
+                print('Запрос отклонён, недостаточно средств')
+                time.sleep(self._pause)
+                if not self._deposit_is_finished:  # Устанавливать блокировку можно только, если депозиты
+                    self._lock.acquire()            # не закончились, иначе это приведет к deadlock
+            else:
+                self._balance -= decrement
+                print(f'Снятие: {decrement}. Баланс: {self._balance}')
                 time.sleep(self._pause)
 
 
